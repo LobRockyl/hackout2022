d94774158d2c67f41b7db87741d7ba92b7c0d7b7
LobRockyl <lobrockyl@gmail.com>
2022-10-16 01:34:09 +0530

    fixes

--- cool_counters/counter/views.py
+++ cool_counters/counter/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Counter
+from pkg_resources import to_filename
+import requests
+import folium
+import traveltimepy as ttpy
+import os
+import json
+from datetime import datetime #for examples
+#store your credentials in an environment variable
+
+os.environ["TRAVELTIME_ID"] = '14e7615d'
+os.environ["TRAVELTIME_KEY"] = 'ff3fa17dc4ac511b62a1f5019c8dd66d'
 
 def index(request):
     if len(Counter.objects.filter(key='counter')) == 0:
@@ -12,4 +22,44 @@ def index(request):
     counter.save()
     context = {'value': counter.value}
     return render(request, 'counter/index.html', context)
+
+def makeMap(request):
+    result = dict()
+    post_data = json.loads(request.body)
+    try:
+        frm = post_data["fromLocation"]
+        to = post_data["toLocation"]
+        vehicle_type = post_data["vehicleType"]
+
+        frm_gc = ttpy.geocoding(frm)
+        to_gc = ttpy.geocoding(to)
+        print(frm_gc)
+        frmlatlongarr = frm_gc["features"][0]["geometry"]["coordinates"]
+        tolatlongarr = to_gc["features"][0]["geometry"]["coordinates"]
+
+        
+        #create api_object 
+
+        resp = requests.post('https://api.traveltimeapp.com/v4/routes', data=api_object, timeout=10)
+
+        a = resp["results"][0]["locations"][0]["properties"][0]["route"]["parts"]
+        c = []                              
+        for i in a:
+            for j in i["coords"]:
+                c.append(j["lat"],j["lng"])
+        print(c)          #plot
+        
+        map = folium.Map((c[0][1],c[0][0]), zoom_start=13)
+        for pt in c:
+            marker = folium.Marker([pt[1], pt[0]]) #latitude,longitude
+            map.add_child(marker)
+        map.save("map2.html")
+
+        result["htmlmap"] = map.get_root().render()
+
+    except Exception as e:
+        result['result'] = "FAILURE"
+        result['exist'] = "NO"
+        result['error_reason'] = str(e)
+    return JsonResponse(result)
 
